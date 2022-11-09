709afeedd81ad39b38e0461da5df867f43b7704f
zhuliana <zhuliana@students.undip.ac.id>
2022-11-09 07:24:46 +0700

    fix nih

--- app/src/main/java/id/ifundip/servicedemo/MainActivity.java
+++ app/src/main/java/id/ifundip/servicedemo/MainActivity.java
@@ -5,13 +5,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+
 
 import java.util.List;
 
--- app/src/main/java/id/ifundip/servicedemo/SongAdapter.java
+++ app/src/main/java/id/ifundip/servicedemo/SongAdapter.java
@@ -12,13 +12,17 @@ import java.util.List;
 
 public class SongAdapter extends RecyclerView.Adapter<SongAdapter.ViewHolder>{
 
+    // Membuat variabel untuk menyimpan data lagu dan menerima inputan click
     private final List<Song> songs;
     private OnItemClickListener listener;
 
+    // Menampilkan data lagu
     public static class ViewHolder extends RecyclerView.ViewHolder{
+        // Menyimpan icon dalam ImageView dan title dalam TextView
         public ImageView icon;
         public TextView title;
 
+        // Mendapatkan icon dan title dari file xml
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
@@ -27,6 +31,7 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.ViewHolder>{
         }
     }
 
+    // Menerima inputan dan disimpan
     public SongAdapter(List<Song> songs, OnItemClickListener listener) {
         this.songs = songs;
         this.listener = listener;
@@ -34,11 +39,13 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.ViewHolder>{
 
     @NonNull
     @Override
+    // Membuat tampilan baru dengan return yang diberikan
     public SongAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.song_row_items,parent,false));
     }
 
     @Override
+    // Membuat tampilan yang berisi posisi, icon dan title
     public void onBindViewHolder(@NonNull SongAdapter.ViewHolder holder, int position) {
         Song song = songs.get(position);
         holder.icon.setImageResource(song.getIcon());
